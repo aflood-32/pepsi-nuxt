e97efae4a62080de3b34c46e143b2186a6b2829e
a.krivopust <afloode@gmail.com>
2020-10-08 22:20:14 +0300

    initial

---
+++ theme/js/script.js
@@ -0,0 +1,44 @@
+$(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+    $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+
+    /* init animation */
+    $('body').addClass('loaded');   
+
+});
+
+$(document).ready(function() {
+
+	/* menu */
+	$('.nav-trigger, #menu-hide').on('click', function(){
+		$('#nav').toggleClass('active');
+		$('.nav-trigger').toggleClass('active');
+		$('body').toggleClass('menu-active');
+		return false;
+	});
+
+	/* main-text */
+	$repeatedText = $('#mainTitleB').text();
+	$repeatedText = $repeatedText + $repeatedText + $repeatedText;
+	$('#mainTitleBefore').text($repeatedText);
+	$('#mainTitleAfter').text($repeatedText);
+
+	/* key */
+	$('.over-key').on('click', function(){
+		$('.key-form-wrap').addClass('active');
+		return false;
+	});
+
+	/**/
+	$(".scroll-box").mCustomScrollbar({});
+
+	/* tooltip */
+	$('[data-toggle="tooltip"]').tooltip({
+		html: true,
+		container: 'body',
+		template: '<div class="tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+	});
+
+});
